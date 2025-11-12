b5b914ce0206d60e6e572f80de4ffc11d120eb42
Goncalo Silva <goncalofmsilva2004@gmail.com>
2025-11-12 00:46:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
